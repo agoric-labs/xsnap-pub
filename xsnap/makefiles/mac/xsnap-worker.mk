9bf37bf6f63bbc37480c2c21187778dd8dd5183e
Kris Kowal <kris@cixar.com>
2022-05-16 20:43:46 -0700

    Build with mxNoConsole=1 unconditionally (#23)
    Refs https://github.com/Agoric/agoric-sdk/issues/5376

--- xsnap/makefiles/mac/xsnap-worker.mk
+++ xsnap/makefiles/mac/xsnap-worker.mk
@@ -35,6 +35,7 @@ C_OPTIONS = \
 	-DXSNAP_TEST_RECORD=0 \
 	-DmxMetering=1 \
 	-DmxDebug=1 \
+	-DmxNoConsole=1 \
 	-DmxBoundsCheck=1 \
 	-DmxParse=1 \
 	-DmxRun=1 \
@@ -53,7 +54,7 @@ endif
 ifeq ($(GOAL),debug)
 	C_OPTIONS += -g -O0 -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-parameter
 else
-	C_OPTIONS += -DmxNoConsole=1 -O3
+	C_OPTIONS += -O3
 endif
 ifeq ($(XSNAP_RANDOM_INIT),1)
 	C_OPTIONS += -DmxSnapshotRandomInit
