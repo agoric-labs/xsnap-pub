3a174c6d01aaacb496775e9fae24bd8cf60c0f36
Richard Gibson <richard.gibson@gmail.com>
2024-07-08 15:25:40 -0400

    refactor: Support EXTRA_DEPS for building `xsnap` executables
    This aligns with the `xsnap-worker` build process.

--- xsnap/makefiles/lin/xsnap.mk
+++ xsnap/makefiles/lin/xsnap.mk
@@ -7,6 +7,8 @@ ifneq ($(VERBOSE),1)
 MAKEFLAGS += --silent
 endif
 
+EXTRA_DEPS =
+
 BUILD_DIR = $(CURDIR)/../../build
 TLS_DIR = $(CURDIR)/../../sources
 
@@ -132,6 +134,7 @@ $(OBJECTS): $(SRC_DIR)/xsAll.h
 $(OBJECTS): $(SRC_DIR)/xsScript.h
 $(OBJECTS): $(SRC_DIR)/xsSnapshot.h
 $(OBJECTS): $(INC_DIR)/xs.h
+$(OBJECTS): $(EXTRA_DEPS)
 $(TMP_DIR)/%.o: %.c
 	@echo "#" $(NAME) $(GOAL) ": cc" $(<F)
 	$(CC) $< $(C_OPTIONS) -c -o $@
